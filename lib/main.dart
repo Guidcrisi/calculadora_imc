2ed00b238bc2d19760bce6b9753110bfe5628ae0
Guilherme Crisi <58445724+Guidcrisi@users.noreply.github.com>
2021-08-26 15:50:29 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: "Contador de Pessoas", home: Home()));
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
 
 class Home extends StatefulWidget {
@@ -10,75 +12,121 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _people = 0;
-  String _infoText = "Pode Entrar";
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
 
-  void _changePeople(int delta) {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _infoText = "Informe seus dados";
+
+  void _resetFileds() {
     setState(() {
-      _people += delta;
+      _infoText = "Informe seus dados";
+      _formKey = GlobalKey<FormState>();
+    });
+    weightController.text = "";
+    heightController.text = "";
+  }
 
-      if (_people < 0) {
-        _infoText = "Como assim???";
-      } else if (_people <= 10) {
-        _infoText = "Pode Entrar";
-      } else {
-        _infoText = "Lotado!";
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente acima do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade grau 1 (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade grau 2 (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade grau 3 (${imc.toStringAsPrecision(3)})";
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Image.asset(
-          "images/restaurant.jpg",
-          fit: BoxFit.cover,
-          height: 1000.0,
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+          actions: <Widget>[
+            IconButton(onPressed: _resetFileds, icon: Icon(Icons.refresh))
+          ],
         ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Pessoas: $_people",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: TextButton(
-                      onPressed: () {
-                        _changePeople(1);
-                      },
-                      child: Text("+1",
-                          style:
-                              TextStyle(color: Colors.white, fontSize: 40.0))),
+                Icon(Icons.person_outline_rounded,
+                    size: 120.0, color: Colors.purple),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Peso (KG)",
+                      labelStyle: TextStyle(color: Colors.purple)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.purple, fontSize: 20.0),
+                  controller: weightController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "insira seu peso";
+                    }
+                  },
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Altura (CM)",
+                      labelStyle: TextStyle(color: Colors.purple)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.purple, fontSize: 20.0),
+                  controller: heightController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "insira sua altura";
+                    }
+                  },
                 ),
                 Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: TextButton(
+                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: Container(
+                    height: 50.0,
+                    child: ElevatedButton(
                       onPressed: () {
-                        _changePeople(-1);
+                        if (_formKey.currentState!.validate()) {
+                          _calculate();
+                        }
                       },
-                      child: Text("-1",
-                          style:
-                              TextStyle(color: Colors.white, fontSize: 40.0))),
+                      child: Text(
+                        "Calcular",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(primary: Colors.purple),
+                    ),
+                  ),
                 ),
+                Text(
+                  _infoText,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.purple, fontSize: 20.0),
+                )
               ],
             ),
-            Text(
-              _infoText,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30.0),
-            ),
-          ],
-        )
-      ],
-    );
+          ),
+        ));
   }
 }
